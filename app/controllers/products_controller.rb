32f0b9ad636f90d25991a0d4f4cebf91853bf8b8
adampdavidson <adam.davidson94@gmail.com>
2013-09-10 08:39:02 -0400

    refactoring products controller to respond to JSON

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,39 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+
+    respond_to do |format|
+      format.html #index.html.erb
+      format.json { render json: @products}
+    end
   end
 
   def show
     @product = Product.find(params[:id])
+
+    respond_to do |format|
+      format.html #index.html.erb
+      format.json { render json: @product}
   end
 
   def new
     @product = Product.new
+
+    respond_to do |format|
+      format.html #index.html.erb
+      format.json { render json: @product}
   end
 
   def create
     @product = Product.new(params[:product])
 
+    respond_to do |format|
     if @product.save
-      redirect_to products_url
+      format.html { redirect_to @product, notice: 'Product was successfully created'}
+      format.json { render json: @product, status: :created, location: @product }
     else
-      render :new
+      format.html { render action: "new" }
+      format.json { render json: @products.errors, status: :unprocessable_entity }
     end
   end
 
@@ -28,15 +44,22 @@ class ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
 
+    respond_to do |format|
     if @product.update_attributes(params[:product])
-      redirect_to @product
+      format.html { redirect_to @product, notice: 'Product was successfully updated'}
+      format.json { head :no_content }
     else
-      render :edit
+      format.html { render action: "edit" }
+      format.json { render json: @products.errors, status: :unprocessable_entity }
     end
   end
 
   def destroy
     @product = Product.find(params[:id])
-    @product.destroy    
+    @product.destroy   
+
+      respond_to do |format|
+      format.html { redirect_to products_url }
+      format.json { head :no_content } 
   end
 end
